0ffe94da1710a947317ec93b13b1921ac8f8c8f0
ChristianPohlschneider <c.pohlschneider@t-online.de>
2026-06-05 15:17:20 +0200

    added product-form

---
+++ src/app/shared/components/product-form/product-form.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-product-form',
+  imports: [ReactiveFormsModule],
+  templateUrl: './product-form.html',
+  styleUrl: './product-form.scss',
+})
+export class ProductForm {
+    productForm = new FormGroup({
+    name: new FormControl('n/a'),
+    description: new FormControl('n/a'),
+    // specs: new FormControl('n/a'),
+    stock: new FormControl('0'),
+    price: new FormControl('0.00'),
+    // date: new FormControl(''),
+  });
+
+  onSubmit() {
+    console.log(this.productForm.value);
+    
+  }
+}
